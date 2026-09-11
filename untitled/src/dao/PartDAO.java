00dbd47f5faa8373fd3a85a24c8e2e8d187892fc
Favourduck <ogunmoyinf@gmail.com>
2026-09-10 22:49:26 -0500

    Update PartDAO.java

--- untitled/src/dao/PartDAO.java
+++ untitled/src/dao/PartDAO.java
@@ -9,21 +9,24 @@ public class PartDAO {
 
     public PartDAO() {
         // Performance (Category 101)
-        parts.add(new Part(1, 101, "V6 Turbo Engine Upgrade", 3500.00, "v6_turbo.png"));
-        parts.add(new Part(2, 101, "V8 Dual-Turbo Engine Upgrade", 6000.00, "v8_turbo.png"));
-        parts.add(new Part(3, 101, "Electric Dual-Motor Pack", 5000.00, "ev_pack.png"));
+        parts.add(new Part(1, 101, "RB26 N1 Twin Turbo Upgrade", 12000.00, "rb26_turbo.png"));
+        parts.add(new Part(2, 101, "2JZ Big Single Turbo Kit", 8500.00, "2jz_turbo.png"));
+        parts.add(new Part(3, 101, "K20A Engine Swap Kit", 6500.00, "k20a_swap.png"));
+        parts.add(new Part(4, 101, "EJ20 Forged Internals Kit", 5500.00, "ej20_forged.png"));
+        parts.add(new Part(5, 101, "Tomei Titanium Exhaust", 1200.00, "tomei_exhaust.png")); // Universal
 
         // Exterior (Category 102)
-        parts.add(new Part(4, 102, "Carbon Fiber Spoiler", 800.00, "spoiler.png"));
-        parts.add(new Part(5, 102, "Matte Black Paint Finish", 1200.00, "matte_black.png"));
+        parts.add(new Part(6, 102, "Voltex Carbon Fiber GT Wing", 1800.00, "voltex_wing.png"));
+        parts.add(new Part(7, 102, "Midnight Purple III Paint", 4500.00, "midnight_purple.png"));
 
         // Interior (Category 103)
-        parts.add(new Part(6, 103, "Premium Leather Seats", 1500.00, "leather.png"));
-        parts.add(new Part(7, 103, "Bose Surround Sound System", 1100.00, "bose.png"));
+        parts.add(new Part(8, 103, "Bride Zeta III Racing Seats", 2200.00, "bride_seats.png"));
+        parts.add(new Part(9, 103, "Nardi Classic Steering Wheel", 400.00, "nardi_wheel.png"));
 
         // Wheels (Category 104)
-        parts.add(new Part(8, 104, "18-inch Alloy Wheels", 900.00, "alloy18.png"));
-        parts.add(new Part(9, 104, "20-inch Forged Performance Rim", 1800.00, "forged20.png"));
+        parts.add(new Part(10, 104, "17-inch Enkei RPF1", 1200.00, "enkei_rpf1.png"));
+        parts.add(new Part(11, 104, "18-inch Volk Racing TE37", 3200.00, "volk_te37.png"));
+        parts.add(new Part(12, 104, "19-inch Work Meister S1", 3800.00, "work_meister.png"));
     }
 
     public List<Part> getAllParts() {
